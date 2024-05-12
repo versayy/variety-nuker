7271c4c541f1a9914dd829f260fb408469a2c2d2
versayy <anthianthyc@gmail.com>
2024-05-12 17:19:51 +0200

    Variety Nuker
    First release

---
+++ setup.bat
@@ -0,0 +1,24 @@
+cls
+chcp 65001 > nul
+@echo off > nul
+cls
+echo.
+echo █ █▄ █ █▀ ▀█▀ ▄▀█ █   █   █ █▄ █ █▀▀   █   █ █▄▄ █▀█ ▄▀█ █▀█ █ █▀▀ █▀
+echo █ █ ▀█ ▄█  █  █▀█ █▄▄ █▄▄ █ █ ▀█ █▄█   █▄▄ █ █▄█ █▀▄ █▀█ █▀▄ █ ██▄ ▄█
+
+pip install discord.py colorama pystyle logging > nul
+
+pip install colorama
+
+pip install pystyle
+
+pip install logging
+
+pip install wmi
+
+
+cls
+color 2
+echo █▀ █ █ █▀▀ █▀▀ █▀▀ █▀ █▀ █▀▀ █ █ █   █   █▄█   █ █▄ █ █▀ ▀█▀ ▄▀█ █   █   █▀▀ █▀▄
+echo ▄█ █▄█ █▄▄ █▄▄ ██▄ ▄█ ▄█ █▀  █▄█ █▄▄ █▄▄  █    █ █ ▀█ ▄█  █  █▀█ █▄▄ █▄▄ ██▄ █▄▀
+timeout 2 > nul
