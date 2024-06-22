c045defd5e8bbee15575c270cbefeb402ac57263
versayy <anthianthyc@gmail.com>
2024-06-22 14:26:31 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -14,7 +14,27 @@ pip install pystyle
 
 pip install logging
 
+pip install subprocess
+
+pip install customtkinter
+
+pip install shutil
+
+pip install base64
+
+pip install sqlite3
+
+pip install socket
+
+pip install zipfile
+
+pip uninstall crypto 
+
+pip uninstall pycrypto
+
 pip install pycryptodome
+
+pip install win32crypt
 
 cls
 color 2
